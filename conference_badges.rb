0057625ce3bd452f6cb4d1a78ff5ef243290b690
Srija S <srijakumar7@gmail.com>
2018-04-15 16:32:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,8 +11,8 @@ def batch_badge_creator(attendees)
     return "Hello, my name is #{attendee}."
 end
 
-
-def assign_rooms (rooms, array)
-  counter = 1
-  room.
+def assign_rooms(attendees)
+  attendees.each_with_index.map do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+  end
 end
